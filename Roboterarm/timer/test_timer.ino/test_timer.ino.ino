4454adc402c115b262d6028b7a3919c2dc82fd52
Armelo2000 <76920586+Armelo2000@users.noreply.github.com>
2021-05-10 02:10:34 +0200

    timer updated

--- Roboterarm/timer/test_timer.ino/test_timer.ino.ino
+++ Roboterarm/timer/test_timer.ino/test_timer.ino.ino
@@ -2,15 +2,20 @@ long i = 1;
 void setup() {
  cli(); // disable interrupts
 //PORTH = (1<<PH5);
-DDRH = (1<<DDH5);
+//DDRH = (1<<DDH5);
 DDRB = (1<<DDB5)|(1<<DDB6)|(1<<DDB7);
 
  // reset
  TCCR1A = 0; // set TCCR1A register to 0
  TCCR1B = 0; // set TCCR1B register to 0
  TCNT1 = 0; // reset counter value
- 
- OCR1A = 15625; //31249; // compare match register
+
+ /* Values in Output compare register with 16MHz clock and 1024 prescaler
+  *  - 3125 --> for each 200ms
+  *  - 15625 --> for each 1s
+  *  - 31250--> for each 2s
+  */
+ OCR1A = 15625; //3125 for 200ms in
  // set prescaler
  TCCR1B |= (1 << CS12) | (1 << CS10); //Prescale 1024
 // 16MHz/1024=15625Hz mit OCR1A=31249 Interrupt alle 2s
